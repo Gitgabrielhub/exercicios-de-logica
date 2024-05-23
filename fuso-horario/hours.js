64200f19c6cfe67f4e46c90c7441bc05e4943c2f
Gabriel Fonseca da Silva <gabrielfonseca@sestsenat.org.br>
2024-05-23 01:52:44 -0300

    inclusão do exercicio de mudar valores de um pais para outro

---
+++ fuso-horario/hours.js
@@ -0,0 +1,22 @@
+let brazil = document.querySelector('#brazil').value
+let franch = document.querySelector('#franch').value
+let resposta = document.querySelector('.resposta')
+let converte = document.querySelector('#converte')
+let reposta = document.querySelector('#resp')
+
+
+let date = new Date();
+let horasBr = `${date.getHours()}:${date.getMinutes()}`
+
+console.log(horasBr)
+
+converte.addEventListener('click',(event)=>{
+    event.preventDefault();
+
+    let brasil = Number(brasil)
+    console.log(brasil)
+    
+   
+
+})
+
