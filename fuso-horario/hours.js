249b022b783a5b075f5af62e0d2dc6e7689f2713
Gabriel Fonseca da Silva <gabrielfonseca@sestsenat.org.br>
2024-05-24 00:47:55 -0300

    alterações no js e no html

--- fuso-horario/hours.js
+++ fuso-horario/hours.js
@@ -1,20 +1,24 @@
-let brazil = document.querySelector('#brazil').value
-let franch = document.querySelector('#franch').value
+let brazil = document.querySelector('#brazil')
 let resposta = document.querySelector('.resposta')
 let converte = document.querySelector('#converte')
-let reposta = document.querySelector('#resp')
+let resp = document.querySelector('#resp')
 
 
 let date = new Date();
 let horasBr = `${date.getHours()}:${date.getMinutes()}`
+let horasfranca = `${date.getHours() + 5}:${date.getMinutes()}`
 
 console.log(horasBr)
 
 converte.addEventListener('click',(event)=>{
     event.preventDefault();
+    
+    let brasa = Number(brazil.value) + 5;
+    
+    
+    return resp.innerHTML = `o horario no brasil é:${horasBr} horario da na frança é ${horasfranca}h`
 
-    let brasil = Number(brasil)
-    console.log(brasil)
+    
     
    
 
